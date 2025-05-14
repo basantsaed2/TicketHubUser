22ca153df960615389bea0727c30ba98c9f1b4dc
basantsaed2 <basantsaed718@gmail.com>
2025-05-14 17:17:28 +0300

    commit

--- src/Router.jsx
+++ src/Router.jsx
@@ -3,7 +3,8 @@ import App from "./App";
 import ProtectedLogin from "./ProtectedData/ProtectedRoute";
 import LoginPage from "./Pages/Authentication/LoginPage";
 import SignUpPage from "./Pages/Authentication/SignUpPage";
-import { CheckoutPage, HomePage, SearchResultPage,ProfilePage, WalletPage, PointsPage ,MyTripsPage, TripDetailsPage } from "./Pages/allPages";
+import { CheckoutPage, HomePage, SearchResultPage, ProfilePage, WalletPage, PointsPage, MyTripsPage, TripDetailsPage } from "./Pages/allPages";
+import Suppert from "./Pages/Support";
 
 export const router = createBrowserRouter(
   [
@@ -11,61 +12,65 @@ export const router = createBrowserRouter(
       path: '',
       element: <App />,
       children: [
-      // Authentication Routes
-      {
-        path: 'auth',
-        element: <ProtectedLogin />,
-        children: [
-          {
-            path: "login",
-            element: <LoginPage />,
-          },
-          {
-            path: "sign_up",
-            element: <SignUpPage />,
-          },
-        ],
-      },
+        // Authentication Routes
+        {
+          path: 'auth',
+          element: <ProtectedLogin />,
+          children: [
+            {
+              path: "login",
+              element: <LoginPage />,
+            },
+            {
+              path: "sign_up",
+              element: <SignUpPage />,
+            },
+          ],
+        },
+        {
+          path: "support",
+          element: <Suppert />,
+        },
 
-      {
-        path: '/',
-        element: <HomePage />,
-      },
-      {
-        path:"trips",
-        element:<Outlet/>,
-        children: [
-          {
-            path:"",
-            element:<SearchResultPage/>,
-          },
-          {
-            path:"details/:tripId",
-            element:<TripDetailsPage/>,
-          },
-        ]
-      },
-      {
-        path:"checkout",
-        element:<CheckoutPage/>
-      },
-      {
-        path:"profile",
-        element:<ProfilePage/>
-      },
-      {
-        path:"wallet",
-        element:<WalletPage/>
-      },
-      {
-        path:"points",
-        element:<PointsPage/>
-      },
-      {
-        path:"my_trips",
-        element:<MyTripsPage/>
-      }
-      
+        {
+          path: '/',
+          element: <HomePage />,
+        },
+        {
+          path: "trips",
+          element: <Outlet />,
+          children: [
+            {
+              path: "",
+              element: <SearchResultPage />,
+            },
+            {
+              path: "details/:tripId",
+              element: <TripDetailsPage />,
+            },
+          ]
+        },
+        {
+          path: "checkout",
+          element: <CheckoutPage />
+        },
+        {
+          path: "profile",
+          element: <ProfilePage />
+        },
+        {
+          path: "wallet",
+          element: <WalletPage />
+        },
+        {
+          path: "points",
+          element: <PointsPage />
+        },
+        {
+          path: "my_trips",
+          element: <MyTripsPage />
+        }
+
       ],
     },
     // {
